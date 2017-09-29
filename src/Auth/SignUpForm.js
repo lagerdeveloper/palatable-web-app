9bba301804b1049f66e746325c81a07952760cde
Alex Lagerman <lagerman.alex@gmail.com>
2017-09-29 16:44:17 -0500

    Start work on sign_up form

--- src/Auth/SignUpForm.js
+++ src/Auth/SignUpForm.js
@@ -1,11 +1,36 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
+import { Input } from 'semantic-ui-react';
+
+
+const NameInput = ({ input, meta }) => {
+  return (
+    <Input
+      icon='user'
+      iconPosition='left'
+      placeholder='Name'
+      {...input}
+    />
+  );
+};
+
+const EmailInput = ({ input, meta }) => {
+  return (
+    <Input
+      icon='mail outline'
+      iconPosition='left'
+      placeholder='Email'
+      {...input}
+    />
+  );
+};
 
 const SignUpForm = (props) => {
   const { handleSubmit } = props;
   return (
     <form onSubmit={handleSubmit}>
-      <Field name='name' component='input' placeholder='Name' />
+      <Field name='name' component={NameInput} />
+      <Field name='email' component={EmailInput} />
     </form>
   );
 };
