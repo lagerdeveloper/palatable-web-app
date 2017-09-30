5bdab13cb96ebcbc44ee45cd507c86aaf8d276ec
Alex Lagerman <lagerman.alex@gmail.com>
2017-09-29 23:15:09 -0500

    Finish some styling for the Sign Up Form

--- src/Auth/SignUpForm.js
+++ src/Auth/SignUpForm.js
@@ -1,37 +1,65 @@
 import React from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { Input } from 'semantic-ui-react';
+import { Form, Input, Button, Grid, Segment } from 'semantic-ui-react';
 
 
 const NameInput = ({ input, meta }) => {
   return (
-    <Input
-      icon='user'
-      iconPosition='left'
-      placeholder='Name'
-      {...input}
-    />
+    <Form.Field>
+      <Input
+        icon='user'
+        iconPosition='left'
+        placeholder='Name'
+        type='text'
+        {...input}
+      />
+    </Form.Field>
   );
 };
 
 const EmailInput = ({ input, meta }) => {
   return (
-    <Input
-      icon='mail outline'
-      iconPosition='left'
-      placeholder='Email'
-      {...input}
-    />
+    <Form.Field>
+      <Input
+        icon='mail outline'
+        iconPosition='left'
+        placeholder='Email'
+        type='email'
+        {...input}
+      />
+    </Form.Field>
+  );
+};
+
+const PasswordInput = ({ input, meta }) => {
+  return (
+    <Form.Field>
+      <Input
+        icon='lock'
+        iconPosition='left'
+        placeholder='Password'
+        type='password'
+        {...input}
+      />
+    </Form.Field>
   );
 };
 
 const SignUpForm = (props) => {
   const { handleSubmit } = props;
   return (
-    <form onSubmit={handleSubmit}>
-      <Field name='name' component={NameInput} />
-      <Field name='email' component={EmailInput} />
-    </form>
+    <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+      <Grid.Column style={{ maxWidth: 450 }}>
+        <Form size='large' onSubmit={handleSubmit}>
+          <Segment raised padded='very'>
+            <Field name='name' component={NameInput} />
+            <Field name='email' component={EmailInput} />
+            <Field name='password' component={PasswordInput} />
+            <Button primary fluid size='large' type='submit'>Sign Up</Button>
+          </Segment>
+        </Form>
+      </Grid.Column>
+    </Grid>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './semantic/dist/semantic.min.css';
 import './index.css';
+import './semantic/dist/semantic.min.css';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './configStore';
