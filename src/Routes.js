f19e1b8d4b84fcbf5f6f892f4e27741e36d6d7fe
Alex Lagerman <lagerman.alex@gmail.com>
2017-09-30 01:35:47 -0500

    Improve Routes.js by returning array of routes

--- src/Routes.js
+++ src/Routes.js
@@ -3,14 +3,12 @@ import { Route } from 'react-router-dom';
 import SignUpContainer from './Auth/SignUpContainer';
 
 const Routes = (props) => {
-  return (
-    <div>
-      <Route exact path='/' render={() => <p>HOME</p>} />
-      <Route path='/drinks' render={() => <p>DRINKS</p>} />
-      <Route path='/login' render={() => <p>LOGIN</p>} />
-      <Route path='/sign_up' component={SignUpContainer} />
-    </div>
-  );
+  return [
+      <Route exact path='/' render={() => <p>HOME</p>} />,
+      <Route path='/drinks' render={() => <p>DRINKS</p>} />,
+      <Route path='/login' render={() => <p>LOGIN</p>} />,
+      <Route path='/sign_up' component={SignUpContainer} />,
+  ];
 };
 
 export default Routes;
