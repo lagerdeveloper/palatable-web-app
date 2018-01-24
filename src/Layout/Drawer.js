d9d793e6068090a9e7043602b27d204e8d33090c
Alex Lagerman <lagerman.alex@gmail.com>
2018-01-23 23:23:30 -0600

    Start work on custom drawer functionality

--- src/Layout/Drawer.js
+++ src/Layout/Drawer.js
@@ -1,50 +1,107 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Sidebar, Menu, Icon, Image } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import logo from '../Images/palatable_red_logo.svg';
 import './Layout.css';
+import './Drawer.css';
 
-const Drawer = (props) => {
-  const { toggleSideBar, sideBarVisible, activeMenuItem, handleMenuItemClick } = props;
-  return (
-    <Sidebar
-      as={Menu}
-      animation='overlay'
-      className="drawer"
-      size='huge'
-      visible={sideBarVisible}
-      vertical
-    >
-      <Menu.Item
-        className='removeIcon'
-        onClick={toggleSideBar}
-        style={{ height: 59.69 }}
-      >
-        <Icon name='remove' className='left' />
-        <Image src={logo} />
-      </Menu.Item>
-      <Menu.Item
-        as={Link}
-        to='/'
-        name='home'
-        active={activeMenuItem === 'home'}
-        onClick={handleMenuItemClick}
-      >
-        <Icon name='home' className='left' />
-        Home
-      </Menu.Item>
-      <Menu.Item
-        as={Link}
-        to='/cocktails'
-        name='cocktails'
-        active={activeMenuItem === 'cocktails'}
-        onClick={handleMenuItemClick}
-      >
-        <Icon name='cocktail' className="left" />
-        Cocktails
-      </Menu.Item>
-    </Sidebar>
-  );
-};
+function isChildOfClass(target, parentClass) {
+  //If node is root then we have reached root of tree
+  if (target.id === 'root') {
+    return false;
+  }
+  if (target.classList.contains(parentClass)) {
+    return true;
+  }
+  return isChildOfClass(target.parentNode, parentClass);
+}
+
+class Drawer extends Component {
+  constructor(props) {
+    super(props);
+    this.close = this.close.bind(this);
+    this.bodyClose = this.bodyClose.bind(this);
+    this.state = { open: props.hasOwnProperty('open') ? props.open : false };
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+    if (nextState.open) {
+      document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+      document.body.addEventListener('click', this.bodyClose);
+    } else {
+      document.body.style.backgroundColor = "white";
+      document.body.removeEventListener('click', this.bodyClose);
+    }
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.hasOwnProperty('open')) {
+      this.setState({ open: nextProps.open });
+    }
+  }
+
+  bodyClose(e) {
+    if (!isChildOfClass(e.target, 'drawer')) {
+      this.setState({ open: false });
+    }
+  }
+
+  close() {
+    this.setState({ open: false });
+  }
+
+  render() {
+    const { open } = this.state;
+    return (
+      <div className={`drawer ${open ? 'open' : ''}`}>
+        <Link to='/'>Home</Link>
+        <button onClick={this.close}>Close</button>
+        <p>hello</p>
+      </div>
+    );
+  }
+}
+// const Drawer = (props) => {
+//   const { toggleSideBar, sideBarVisible, activeMenuItem, handleMenuItemClick } = props;
+//   return (
+//     <Sidebar
+//       as={Menu}
+//       animation='overlay'
+//       className="drawer"
+//       size='huge'
+//       visible={sideBarVisible}
+//       vertical
+//     >
+//       <Menu.Item
+//         className='removeIcon'
+//         onClick={toggleSideBar}
+//         style={{ height: 59.69 }}
+//       >
+//         <Icon name='remove' className='left' />
+//         <Image src={logo} />
+//       </Menu.Item>
+//       <Menu.Item
+//         as={Link}
+//         to='/'
+//         name='home'
+//         active={activeMenuItem === 'home'}
+//         onClick={handleMenuItemClick}
+//       >
+//         <Icon name='home' className='left' />
+//         Home
+//       </Menu.Item>
+//       <Menu.Item
+//         as={Link}
+//         to='/cocktails'
+//         name='cocktails'
+//         active={activeMenuItem === 'cocktails'}
+//         onClick={handleMenuItemClick}
+//       >
+//         <Icon name='cocktail' className="left" />
+//         Cocktails
+//       </Menu.Item>
+//     </Sidebar>
+//   );
+// };
 
 export default Drawer;
--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -19,128 +19,145 @@ import './Layout.css';
 class Layout extends Component {
   constructor(props) {
     super(props);
-    this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
-    this.toggleSideBar = this.toggleSideBar.bind(this);
-    this.hideSideBar = this.hideSideBar.bind(this);
-    this.logout = this.logout.bind(this);
-    this.state = {
-      activeMenuItem: 'home',
-      sideBarVisible: false,
-    };
-  }
-
-  handleMenuItemClick(e, { name }) {
-    this.setState({ activeMenuItem: name, sideBarVisible: false });
-  }
-
-  toggleSideBar() {
-    this.setState({ sideBarVisible: !this.state.sideBarVisible });
-  }
-
-  hideSideBar() {
-    this.setState({ sideBarVisible: false });
-  }
-
-  logout() {
-    this.props.destroy('new_recipe');
-    this.props.logout();
+    this.state = { drawerOpen: false };
   }
 
   render() {
-    const { authenticated, login } = this.props;
-    const { activeMenuItem, sideBarVisible } = this.state;
-    const authDropdownOptions = [
-      {
-        key: 'account',
-        text: 'Account',
-        icon: 'user circle',
-        as: Link,
-        to: '/account',
-        active: false,
-        selected: false,
-      },
-      {
-        key: 'sign-out',
-        text: 'Sign Out',
-        icon: 'sign out',
-        onClick: this.logout,
-        active: false,
-        selected: false,
-      },
-    ];
-    const authMenuItems = [
-      <Popup
-        trigger={
-          <Menu.Item
-            as={Link}
-            to='/recipes/new'
-            icon='add'
-          />
-        }
-        content='Add a recipe'
-        key={1}
-      />,
-      <Dropdown
-        key={2}
-        item
-        icon={null}
-        header={login}
-        trigger={<Icon name="user"/>}
-        options={authDropdownOptions}
-      />
-    ];
-    const noAuthMenuItems = [
-      <Menu.Item
-        as={Link}
-        key={1}
-        active={activeMenuItem === 'sign_up'}
-        to='sign_up'
-        name='sign_up'
-        onClick={this.handleMenuItemClick}
-      >
-        Sign Up
-      </Menu.Item>,
-      <Menu.Item
-        as={Link}
-        key={2}
-        active={activeMenuItem === 'sign_in'}
-        to='/sign_in'
-        name='sign_in'
-        onClick={this.handleMenuItemClick}
-      >
-        Sign In
-      </Menu.Item>
-    ];
-    const SubMenuItems = authenticated ? authMenuItems : noAuthMenuItems;
-    return (
-      <div>
-        <Menu
-          borderless
-          size='huge'
-        >
-          <Menu.Item
-            icon='content'
-            onClick={this.toggleSideBar}
-          />
-          <Menu.Item>
-            <Image src={logo} />
-          </Menu.Item>
-          <Menu.Menu position='right'>
-            {SubMenuItems}
-          </Menu.Menu>
-        </Menu>
-        <Drawer
-          activeMenuItem={activeMenuItem}
-          sideBarVisible={sideBarVisible}
-          handleMenuItemClick={this.handleMenuItemClick}
-          toggleSideBar={this.toggleSideBar}
-        />
-        <Sidebar.Pusher className="main" onClick={this.hideSideBar}>
-          <Routes />
-        </Sidebar.Pusher>
+    return [
+      <Drawer key={1} open={this.state.drawerOpen} />,
+      <div key={2} className="layout">
+        <button onClick={() => this.setState({ drawerOpen: true })} >Open</button>
+        <button onClick={() => this.setState({ drawerOpen: false })}>Close</button>
       </div>
-    )
+    ];
   }
 }
+
+// class Layout extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.handleMenuItemClick = this.handleMenuItemClick.bind(this);
+//     this.toggleSideBar = this.toggleSideBar.bind(this);
+//     this.hideSideBar = this.hideSideBar.bind(this);
+//     this.logout = this.logout.bind(this);
+//     this.state = {
+//       activeMenuItem: 'home',
+//       sideBarVisible: false,
+//     };
+//   }
+//
+//   handleMenuItemClick(e, { name }) {
+//     this.setState({ activeMenuItem: name, sideBarVisible: false });
+//   }
+//
+//   toggleSideBar() {
+//     this.setState({ sideBarVisible: !this.state.sideBarVisible });
+//   }
+//
+//   hideSideBar() {
+//     this.setState({ sideBarVisible: false });
+//   }
+//
+//   logout() {
+//     this.props.destroy('new_recipe');
+//     this.props.logout();
+//   }
+//
+//   render() {
+//     const { authenticated, login } = this.props;
+//     const { activeMenuItem, sideBarVisible } = this.state;
+//     const authDropdownOptions = [
+//       {
+//         key: 'account',
+//         text: 'Account',
+//         icon: 'user circle',
+//         as: Link,
+//         to: '/account',
+//         active: false,
+//         selected: false,
+//       },
+//       {
+//         key: 'sign-out',
+//         text: 'Sign Out',
+//         icon: 'sign out',
+//         onClick: this.logout,
+//         active: false,
+//         selected: false,
+//       },
+//     ];
+//     const authMenuItems = [
+//       <Popup
+//         trigger={
+//           <Menu.Item
+//             as={Link}
+//             to='/recipes/new'
+//             icon='add'
+//           />
+//         }
+//         content='Add a recipe'
+//         key={1}
+//       />,
+//       <Dropdown
+//         key={2}
+//         item
+//         icon={null}
+//         header={login}
+//         trigger={<Icon name="user"/>}
+//         options={authDropdownOptions}
+//       />
+//     ];
+//     const noAuthMenuItems = [
+//       <Menu.Item
+//         as={Link}
+//         key={1}
+//         active={activeMenuItem === 'sign_up'}
+//         to='sign_up'
+//         name='sign_up'
+//         onClick={this.handleMenuItemClick}
+//       >
+//         Sign Up
+//       </Menu.Item>,
+//       <Menu.Item
+//         as={Link}
+//         key={2}
+//         active={activeMenuItem === 'sign_in'}
+//         to='/sign_in'
+//         name='sign_in'
+//         onClick={this.handleMenuItemClick}
+//       >
+//         Sign In
+//       </Menu.Item>
+//     ];
+//     const SubMenuItems = authenticated ? authMenuItems : noAuthMenuItems;
+//     return (
+//       <div>
+//         <Menu
+//           borderless
+//           size='huge'
+//         >
+//           <Menu.Item
+//             icon='content'
+//             onClick={this.toggleSideBar}
+//           />
+//           <Menu.Item>
+//             <Image src={logo} />
+//           </Menu.Item>
+//           <Menu.Menu position='right'>
+//             {SubMenuItems}
+//           </Menu.Menu>
+//         </Menu>
+//         <Drawer
+//           activeMenuItem={activeMenuItem}
+//           sideBarVisible={sideBarVisible}
+//           handleMenuItemClick={this.handleMenuItemClick}
+//           toggleSideBar={this.toggleSideBar}
+//         />
+//         <Sidebar.Pusher className="main" onClick={this.hideSideBar}>
+//           <Routes />
+//         </Sidebar.Pusher>
+//       </div>
+//     )
+//   }
+// }
 
 export default Layout;
